c5f6f5e16bfef9ce5a3cfa1667a2e33b960410a3
Rajesh2k22 <102841802+Rajesh2k22@users.noreply.github.com>
2022-12-23 22:29:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+function get(){
+
+     var city= document.getElementById("city").value;
+     console.log(city);
+     if(city.length==0){
+        city="Delhi";
+     }
+     var p= document.getElementById("detail");
+     p.innerHTML=city;
+    var API_key="7e9e005a9a4910089cbf3673d906de45";
+    var str=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`;
+
+    console.log(str);
+
+    fetch(str).then(function (Response){
+          return Response.json();  
+    }).then( function (data){
+
+        console.log(data.main.temp);
+        p.innerHTML= `Temp on ${city} is ${data.main.temp} <sup>o</sup> F`;
+    })
+
+   
+
+
+   
+
+}
+
+// get();
